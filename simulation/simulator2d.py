c425ac317ddae987ffac27459169460cd90e9cfa
Thomas Teisberg <teisberg@stanford.edu>
2016-02-13 19:05:06 -0800

    Updated simulation code

--- simulation/simulator2d.py
+++ simulation/simulator2d.py
@@ -4,14 +4,33 @@ import math
 ex_state = {'up_angle': math.pi / 8,    # angle of the pendulum from vertical
             'up_angle_vel' : 0,         # angular velocity of the above
             'up_angle_acc' : 0,         # angular acceleration of the above
-            'servo_x': math.pi / 16,    # x servo angle 
-            'servo_y': math.pi / 16}    # y servo angle
+            'servo': math.pi / 16,    # servo angle 
 
-dt = 0.001
+dt = 0.001      # seconds
+mass = 30       # grams     - total mass
+mass_rotatable  # grams     - mass of each rotating mass
 
-def simulate_timestep(in_state, dt):
-    #cg = find_cg(in_state['servo_a'], in_state['servo_b']);
-    #torque = calc_torque()
-    # more stuff
-    return in_state
+
+def simulate_timestep(s, dt, x_axis=True):
+    # Calculate CG and moment of inertia
+    cg = find_cg(s['up_angle'], s['servo_x']]);
+    mi = find_mi(s['up_angle'], s['servo_x'], s['servo_y']);
+        
+    
+    # Calculate rotational acceleration and integrate for velocity and angle
+    torque = mass * cg[1]
+    rot_acc = torque * mi
+    rot_vel = (rot_acc * dt) + s['up_angle_vel']
+    rot     = (rot_vel * dt) + s['up_angle']
+    
+    # update state dictionary
+    s['up_angle'] = rot
+    s['up_angle_vel'] = rot_vel
+    s['up_angle_acc'] = rot_acc
+    s['cg'] = cg
+    s['mi'] = mi
+
+    return s
+
+def find_cg(angle, servo_x, servo_y):
 
